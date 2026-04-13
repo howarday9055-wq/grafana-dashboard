1be91f4841afaae3b40509435e9a02f0451171cd
frank <frank@ck-web.com>
2026-04-13 10:50:15 +0000

    add cluster redis-exporter

--- delete_dashboard.sh
+++ delete_dashboard.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+configmaps=$(kubectl get configmaps -n monitoring | grep grafana-dashboard)
 if [[ ${#@} -ne 1 ]];then
     echo "Usage: bash $0 configmap_name"
+    echo -e "Choices: \n$configmaps"
     exit 1
 fi
 cm_name=$1
